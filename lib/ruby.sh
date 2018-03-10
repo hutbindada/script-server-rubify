cf7571d2e6804715327f2bbcb1d214359a7f16d7
hutbindada <hutbindada@gmail.com>
2018-03-10 18:23:45 +0700

    update install rbenv and ruby

--- lib/ruby.sh
+++ lib/ruby.sh
@@ -46,7 +46,13 @@ function install_ntp {
 function install_rbenv
 {
   log "install_rbenv: Installing standard rbenv..."
-  curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  exec $SHELL
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+  exec $SHELL
 }
 
 function fix_locale_rbenv_command {
